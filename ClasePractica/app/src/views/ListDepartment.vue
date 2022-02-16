e4d4e6eae47f16bda8f565c81686742b6847d5be
EduardoJosueAlvarez12 <97970428+EduardoJosueAlvarez12@users.noreply.github.com>
2022-02-16 16:05:58 -0600

    Actualizacion carpetas de tarea

---
+++ ClasePractica/app/src/views/ListDepartment.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Listado de departamentos</h1>
+        <department/>
+    </div>
+</template>
+
+<script>
+import Department from '@/components/Department.vue'
+
+export default{
+    name:'ListDepartment',
+    components:{
+        Department
+    }
+}
+</script>
